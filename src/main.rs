d62bd41ebea91db6569cb96e030635255ac47419
Toast <ThatOneToast@proton.me>
2024-08-22 12:39:33 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use parser::Parser;
-
-
-
 
 mod parser;
 mod expressions;
 mod tests;
 
 
+use std::io::{self, Read};
+
 fn main() {
-    println!("Run cargo test")
+    println!("Welcome to Maths!");
+    println!("When you are finished with your maths script, Unix: Ctrl+D, (Shitty) Windows: Ctrl+Z");
+    
+    loop {
+        
+        let mut input = String::new();
+    
+        print!("Maths> ");
+        io::stdin().read_to_string(&mut input)
+            .expect("Failed to read input");
+        
+        let mut parser = parser::Parser::new(input.as_str());
+        parser.parse();
+    }
 }
